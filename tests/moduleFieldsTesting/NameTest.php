edf820dc35470258471c4f5273249e26f8f498fc
Ganesh Bhosale <gdb.sci123@gmail.com>
2016-10-29 19:01:15 +0530

    NameTest

--- tests/moduleFieldsTesting/NameTest.php
+++ tests/moduleFieldsTesting/NameTest.php
@@ -4,7 +4,7 @@ use Illuminate\Foundation\Testing\WithoutMiddleware;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Illuminate\Foundation\Testing\DatabaseTransactions;
 
-class FieldNameTest extends TestCase
+class NameTest extends TestCase
 {
 	use DatabaseMigrations;
 	// use DatabaseTransactions;
